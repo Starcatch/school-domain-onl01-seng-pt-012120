1f1fe85d86935fba6925ae18862eeba394359fec
Timur Mishiev <timurmishiev@gmail.com>
2020-02-08 20:38:18 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,15 @@
 class School
-  
   attr_reader : name 
+  
   def initialize(name)
     @name = name
+  end 
+  
+  def roster
+    {}
+  end 
+  
+  def add_sudents(some_students)
+    roster << some_students
   end 
 end 
